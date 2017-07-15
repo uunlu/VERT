6923368a330ef8c06b31a8e8a823475fc6d6a63a
Bojan Veljanovski <bojan.veljanovski@haselt.com>
2017-07-16 00:34:23 +0200

    Fix references and NuGet packages in Web project

--- src/VERT.Web/App_Start/SwaggerConfig.cs
+++ src/VERT.Web/App_Start/SwaggerConfig.cs
@@ -1,7 +1,7 @@
 using System.Web.Http;
-using WebActivatorEx;
 using VERT.Web;
 using Swashbuckle.Application;
+using System.Web;
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
